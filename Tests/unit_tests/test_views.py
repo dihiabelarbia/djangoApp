615958ba6f2b1813dc6e8d73d5d70480aed3ad0b
dbelarbia <dbelarbia@assurone-group.com>
2022-11-15 16:05:01 +0100

    first commit

---
+++ Tests/unit_tests/test_views.py
@@ -0,0 +1,39 @@
+"""
+This file demonstrates writing tests using the unittest module.
+"""
+
+import django
+from django.test import TestCase
+import os
+import sys
+
+"""
+When we use Django, we have to tell it which settings we are using. We do this by using an environment variable, DJANGO_SETTINGS_MODULE. 
+This is set in manage.py. We need to explicitly set it for tests to work with pytest.
+"""
+
+sys.path.append(os.path.join(os.getcwd(), 'Django_v'))
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "Django_v.settings"
+)
+django.setup()
+
+class ViewTest(TestCase):
+    """Tests for the application views."""
+
+    if django.VERSION[:2] >= (1, 7):
+        # Django 1.7 requires an explicit setup() when running tests in PTVS
+        @classmethod
+        def setUpClass(cls):
+            super(ViewTest, cls).setUpClass()
+
+    def test_unit_home(self):
+        """Tests the home page."""
+        response = self.client.get('/')
+        self.assertContains(response, 'home', 1, 200)
+
+    def test_unit_logfiles(self):
+        """Tests the about page."""
+        response = self.client.get('/logfiles')
+        self.assertContains(response, 'logfiles', 3, 200)
---
+++ manage.cmd
@@ -0,0 +1,8 @@
+cd C:\Python310\Scripts
+c:
+
+set PYTHONPATH=D:\scripts\test_dihia\Test_Deploiement
+
+c:\Python310\python.exe D:\scripts\test_dihia\Django_v\manage.py runserver 9100
+
+pause
